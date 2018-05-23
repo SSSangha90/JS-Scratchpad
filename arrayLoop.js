e6402a55005c85fd50b3efe4ec64985623e74a44
Sukh Sangha <37102418+SSSangha90@users.noreply.github.com>
2018-05-23 13:18:43 +0100

    Add files via upload
    Looping over an array

---
+++ arrayLoop.js
@@ -0,0 +1,11 @@
+
+
+var friends = ["Mark", "Lisa", "Denny", "Peter", "Steven", "Claudette"];
+
+function greetFriends() {
+  for(var i = 0; i < friends.length; i++){
+    console.log("Hi there " + friends[i]);
+  }
+}
+
+greetFriends();
